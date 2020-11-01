0c783abdcaf8ec6847fa294f80adee22b3d2b820
abelso <aron.belso@gmail.com>
2020-11-02 00:27:12 +0100

    Setup Capybara and DatabaseCleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,12 +48,12 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
   # Rollback database after tests
-  config.around(:each) do |example|
-    ActiveRecord::Base.transaction do
-      example.run
-      raise ActiveRecord::Rollback
-    end
-  end
+  # config.around(:each) do |example|
+  #   ActiveRecord::Base.transaction do
+  #     example.run
+  #     raise ActiveRecord::Rollback
+  #   end
+  # end
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
