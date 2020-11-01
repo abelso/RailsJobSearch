4e45f8efe18381b67990a7e15c1257f1b4bd3519
abelso <aron.belso@gmail.com>
2020-11-01 20:40:48 +0100

    Modified factories

--- spec/factories/application.rb
+++ spec/factories/application.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     first_name { 'Sean' }
     last_name { 'Connery' }
     dob { '1985-12-13' }
-    email { 'aron.belso@gmail.com' }
+    email { 'sean.connery@gmail.com' }
     telephone { '38591111111' }
     address { 'Ilica 1' }
     qualification { 'VSS' }
