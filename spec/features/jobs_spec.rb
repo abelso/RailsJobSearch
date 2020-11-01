d04bbc9ad07a9d8ef785b68f552f5d2cf745d27c
abelso <aron.belso@gmail.com>
2020-11-02 00:28:13 +0100

    Setup Jobs feature spec

---
+++ spec/features/jobs_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "Jobs", type: :feature do
+  feature 'creates a new application' do
+    let!(:job) { FactoryBot.create(:job) }
+
+    scenario 'with successful parameters' do
+      visit "/jobs/#{job.id}"
+      expect(page).to have_content(job.title)
+      within('form') do
+        fill_in('Ime', with: 'Sean')
+        fill_in('Prezime', with: 'Connery')
+        fill_in('Datum rođenja', with: '1930-01-01')
+        fill_in('Email', with: 'sean.connery@gmail.com')
+        fill_in('Broj telefona', with: '385911234567')
+        fill_in('Adresa', with: 'Ilica 1')
+        select('VSS', from: 'Stručna sprema')
+        click_on 'Prijava'
+      end
+      expect(page).to have_content('Application was successfully created.')
+    end
+  end
+
+  
+end
