7de9418be284967d552d122194f0e2f57d6e77e2
abelso <aron.belso@gmail.com>
2020-11-01 17:32:48 +0100

    Added other methods to Job request spec

--- spec/requests/jobs_spec.rb
+++ spec/requests/jobs_spec.rb
@@ -11,12 +11,55 @@ RSpec.describe 'Jobs', type: :request do
     end
   end
 
+  describe 'GET /jobs/:id' do
+    let!(:job) { FactoryBot.create(:job, title: 'Rails Developer', description: 'Some description', name: 'Ingemark', email: 'ingemark@asd.asd', category: 'IT', deadline: '2020-11-15') }
+
+    it 'returns a job' do
+      get "/jobs/#{job.id}"
+
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('Rails Developer')
+    end
+  end
+
+  describe 'GET /jobs/new' do
+    it 'generates a new job' do
+      get '/jobs/new'
+
+      expect_any_instance_of(Job)
+    end
+  end
+
   describe 'POST /jobs' do
     it 'creates a new job' do
-      puts Job.count
       expect {
         post '/jobs', params: { job: { title: 'Rails Developer', description: 'Some description', name: 'Ingemark', email: 'ingemark@asd.asd', category: 'IT', deadline: '2020-11-15' } }
       }.to change { Job.count }.from(Job.count).to(Job.count + 1)
+    end
+
+    it 'fails to create a new job with missing parameter (title)' do
+      expect {
+        post '/jobs', params: { job: { description: 'Some description', name: 'Ingemark', email: 'ingemark@asd.asd', category: 'IT', deadline: '2020-11-15' } }
+      }.not_to change { Job.count }
+    end
+  end
+
+  describe 'PATCH /jobs/:id' do
+    let!(:job) { FactoryBot.create(:job, title: 'Rails Developer', description: 'Some description', name: 'Ingemark', email: 'ingemark@asd.asd', category: 'IT', deadline: '2020-11-15') }
+
+    it 'updates a job' do
+      patch "/jobs/#{job.id}", params: { job: { title: 'Ruby on Rails Developer' } }
+
+      expect(response).to redirect_to(@job)
+      follow_redirect!
+
+      expect(response.body).to include('Ruby on Rails Developer')  
+    end
+
+    it 'fails to update a job with empty title parameter' do
+      patch "/jobs/#{job.id}", params: { job: { title: nil } }
+
+      expect(response.body).to include('Ažuriranje posla')
     end
   end
 
@@ -27,7 +70,6 @@ RSpec.describe 'Jobs', type: :request do
       expect {
         delete "/jobs/#{job.id}"
       }.to change { Job.count }.from(Job.count).to(Job.count - 1)
-      
     end
   end
 end
